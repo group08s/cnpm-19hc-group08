40e430998052eb53d23a3216cbac85466caf6046
Trung Hieu <19424003@student.hcmus.edu.vn>
2020-04-20 00:03:35 +0700

    [CreateModel]/[create model service and serviceCustomer

---
+++ models/Service.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Service = new Schema({
+    MADV: {type: String, required: true, trim: true},
+    Name: {type: String, required: true, trim: true},
+    Price: {type: Number, required: true},
+    Note: {type:String, trim: true}
+}, {collection: "Service"});
+
+const ModelService = mongoose.model("Service", Service);
+module.exports = ModelService;
---
+++ models/ServiceCustomer.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ServiceCustomer = new Schema({
+    IDKH: {type: String, required: true, trim: true},
+    Room: {
+        BookTime: {type: Number},
+        ArrivalTime: {type: Number, required: true},
+        LeaveTime: {type: Number, required: true}
+    },
+    Restaurant: [
+        {
+            MADH: {type: String, required: true, trim: true}
+        }
+    ],
+    Orther: [
+        {
+            MADV: {type: String, required, trim: true},
+            TimeStart: {type: Number, required: true},
+            type: {type: Number}
+        }
+    ]
+}, {collection: "ServiceCustomer"});
+
+const ModelServiceCustomer = mongoose.model("ServiceCustomer", ServiceCustomer);
+module.exports = ModelServiceCustomer;
