d68f2059224e6c2e1b08ac43fc0b048ddf50bf0a
Trung Hieu <19424003@student.hcmus.edu.vn>
2020-04-19 23:12:31 +0700

    [ConectDatabase]/[Create conected into database]

--- app.js
+++ app.js
@@ -4,10 +4,48 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+//router and modal
+const indexRouter = require('./routes/index');
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const ulrDB = 'mongodb://localhost:27017/hotelDB'
+// mongoose.set('useCreateIndex', true)
+mongoose
+  .connect(ulrDB, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=> console.log('DB Conect on port 27017!'));
+db.on('error', err => {
+  console.log('DB connection error: ', err.message);
+})
+// test:
+// const indexSchema = new mongoose.Schema({
+//   name: String,
+// }, { collection: 'nhanvien'})
+// //name colection, schema
+// const user = mongoose.model('nhanvien', indexSchema);
+
+
+
+
+
+// db.once('open', function() {
+//   // we're connected!
+//   console.log("conecct")
+  
+// });
+
+// user.create([
+//   { name: "tit" },
+//   { name: "mit"}
+// ])
+
+// user.find().exec((err, users) =>{
+//   console.log("list users: ", users)
+// })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +57,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
+
+//router into database
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+
+// app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
