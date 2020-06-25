9a6900ecced7eef6b1ccb40386eb8f55bb41c59a
haminhbaotoan <hmbt93@gmail.com>
2020-06-25 13:01:14 +0700

    toan

--- models/ServiceCustomer.js
+++ models/ServiceCustomer.js
@@ -4,18 +4,19 @@ const Schema = mongoose.Schema;
 
 const ServiceCustomer = new Schema({
     IDCustomer : [{type : mongoose.Types.ObjectId , require : true , ref : "Customer"}],
-    IDRoom : {type : String , required : true , trim : true},
-    ArrivalTime: {type: Number, required: true},
-    LeaveTime: {type: Number, required: true},
+    // IDRoom : {type : String , required : true , trim : true},
+    // ArrivalTime: {type: Number, required: true},
+    // LeaveTime: {type: Number, required: true},
     Note : {type : String , required : true, trim : true},
-    // BookRoom: [
-    //     {
-    //         IdRoom: {type: String, required: true, trim: true},
-    //         BookTime: {type: Number},
-    //         ArrivalTime: {type: Number, required: true},
-    //         LeaveTime: {type: Number, required: true}
-    //     }
-    // ],
+    BookRoom: [
+        {
+            IdRoom: {type: String, required: true, trim: true},
+            BookTime: {type: Number},
+            ArrivalTime: {type: Number, required: true},
+            LeaveTime: {type: Number, required: true},
+            Onhay: false
+        }
+    ],
     IDRestaurant: [{type : mongoose.Types.ObjectId , required : true , trim : true}],
     IDService: [{type : mongoose.Types.ObjectId, requireed : true , trim : true}],
     status : {type : String , required : true,trim : true}
